c1426c6db05218a239585a26d4a9aafbb440e1c5
sturner1805 <sturner1805@gmail.com>
2017-05-23 16:13:10 +0100

    Add files via upload

---
+++ SeleniumProject/src/seleniumTests/Shopping.java
@@ -0,0 +1,22 @@
+package seleniumTests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Shopping {
+	
+	private static WebDriver driver;
+	
+	@Before
+	
+	@Test
+	public void test() {
+		driver = new ChromeDriver();
+		
+		fail("Not yet implemented");
+	}
+
+}
